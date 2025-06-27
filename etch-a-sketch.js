8bcf4370d7482b47f8ec44b5b70493c13a86afc3
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-06-27 09:51:36 -0700

    feat: create divs with nested for loop
    Store length and width of grid as global constants as bounds for iteration.

---
+++ etch-a-sketch.js
@@ -0,0 +1,10 @@
+const GRID_LENGTH = GRID_WIDTH = 16;
+
+let grid = document.querySelector(".grid");
+
+for (let i = 0; i < GRID_LENGTH; ++i) {
+    for (let j = 0; j < GRID_WIDTH; ++j) {
+        const square = document.createElement("div");
+        grid.appendChild(square);
+    }
+}
