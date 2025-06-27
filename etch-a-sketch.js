e42d954b52667b89d88bf6e34239d3d0fdf300d5
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-06-27 09:53:24 -0700

    refactor: add class to grid square
    Use class to apply styling for all grid squares.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -5,6 +5,8 @@ let grid = document.querySelector(".grid");
 for (let i = 0; i < GRID_LENGTH; ++i) {
     for (let j = 0; j < GRID_WIDTH; ++j) {
         const square = document.createElement("div");
+        square.classList.add("grid-square");
         grid.appendChild(square);
     }
 }
+
